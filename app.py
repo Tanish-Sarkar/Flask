2425241b1333c47836f479ece29d5d9a567d2897
Tanish-Sarkar <tanishnitch@gmail.com>
2025-08-25 23:17:46 +0530

    first commit

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "this is my first flask app"
+
+@app.route("/about")
+def about():
+    return "This is about page"
+
+@app.route('/contact')
+def contct():
+    return "Contact me from here"
