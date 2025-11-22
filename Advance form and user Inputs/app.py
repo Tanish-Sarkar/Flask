174099c445ac5411dca81b0fe568604da2c0ebca
Tanish-Sarkar <tanishnitch@gmail.com>
2025-11-22 20:27:59 +0530

    flasking the messages

--- Advance form and user Inputs/app.py
+++ Advance form and user Inputs/app.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
-@app.route('/feedback', methods = ['POST', 'GET'])
-def feedback():
+# Manual Dynamic Forms
+
+# @app.route('/feedback', methods = ['POST', 'GET'])
+# def feedback():
+#     if request.method == 'POST':
+#         name = request.form.get("username")   # returns None if not found (prevent KeyError and app crash)
+#         message = request.form.get('message')
+
+#         return render_template("thankyou.html", user = name, message = message)
+
+#     return render_template("feedback.html")
+
+
+app.secret_key = "my-secret-key"
+@app.route('/', methods = ['POST', 'GET'])
+def form():
     if request.method == 'POST':
-        name = request.form.get("username")   # returns None if not found (prevent KeyError and app crash)
-        message = request.form.get('message')
+        name = request.form.get("name")   # returns None if not found (prevent KeyError and app crash)
+        if not name:
+            flash("Name can not be empty")
+            return redirect(url_for('form'))
+        flash(f"Thanks {name}, your feedback was saved")
+        return redirect(url_for("thankyou"))
+    return render_template("form.html")
 
-        return render_template("thankyou.html", user = name, message = message)
-
-    return render_template("feedback.html")
-
+@app.route('/thankyou')
+def thankyou():
+    return render_template("thankyou.html")
