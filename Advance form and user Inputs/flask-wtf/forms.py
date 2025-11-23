aa996f4cd24ff2c82f380372137698116892d55d
Tanish-Sarkar <tanishnitch@gmail.com>
2025-11-23 14:38:53 +0530

    flask wt-forms DONE

--- Advance form and user Inputs/flask-wtf/forms.py
+++ Advance form and user Inputs/flask-wtf/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(message='Name is required')]) # custom error message
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     submit = SubmitField('Register')
